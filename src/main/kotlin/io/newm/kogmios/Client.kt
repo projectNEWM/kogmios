3b3b3c62d6129d25c54327a137e565084527860e
Andrew Westberg <andrewwestberg@gmail.com>
2025-04-09 18:33:02 +0000

    fix: Increase node timeout to 10 secs

--- src/main/kotlin/io/newm/kogmios/Client.kt
+++ src/main/kotlin/io/newm/kogmios/Client.kt
@@ -23,7 +23,7 @@ interface Client : Closeable {
     fun shutdown()
 
     companion object {
-        const val DEFAULT_REQUEST_TIMEOUT_MS = 5000L
+        const val DEFAULT_REQUEST_TIMEOUT_MS = 10_000L
         const val LONG_REQUEST_TIMEOUT_MS = 180_000L // 3 minutes
         const val INSANE_REQUEST_TIMEOUT_MS = 300_000L // 5 minutes
         const val LUDICROUS_REQUEST_TIMEOUT_MS = 600_000L // 10 minutes
